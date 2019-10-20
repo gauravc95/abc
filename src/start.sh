8f0d6b5675f7d62fbf77f60979ebcf5929ce764e
Jan Christoph Ebersbach <jceb@e-jc.de>
2019-10-20 13:25:20 +0200

    Don't run as non-root

--- src/start.sh
+++ src/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd "$(dirname "${0}")"
 
-if [ $# -gt 1 ]; then
-    _UIDGUID=$(stat -c %u:%g .) docker-compose "${@}"
-else
-    _UIDGUID=$(stat -c %u:%g .) docker-compose up
-fi
+# if [ $# -gt 1 ]; then
+#     _UIDGUID=$(stat -c %u:%g .) docker-compose "${@}"
+# else
+#     _UIDGUID=$(stat -c %u:%g .) docker-compose up
+# fi
+docker-compose up
